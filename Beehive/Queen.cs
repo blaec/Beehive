1695ad23e018c8e949deb3398c751938ef0e937b
blaec <odeskonst@yandex.ru>
2020-10-06 22:26:39 +0300

    Add project files.

---
+++ Beehive/Queen.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beehive
+{
+    class Queen
+    {
+        private Worker[] workers;
+
+        private int shiftNumber = 0;
+
+        private const string NEW_LINE = "\r\n";
+
+        public Queen(Worker[] workers)
+        {
+            this.workers = workers;
+        }
+
+        public bool AssignWork(string job, int numberOfShifts)
+        {
+            bool result = false;
+            for (int i = 0; i < workers.Length; i++)
+            {
+                if (workers[i].DoThisJob(job, numberOfShifts))
+                {
+                    result = true;
+                    break;
+                }
+            }
+            return result;
+        }
+
+        public string WorkTheNextShift()
+        {
+            shiftNumber++;
+            string report = $"Report for shift #{shiftNumber}{NEW_LINE}";
+            for (int i = 0; i < workers.Length; i++)
+            {
+                string worker = $"Worker #{i + 1}";
+                if (workers[i].DidYouFinish())
+                {
+                    report += $"{worker} finished the job{NEW_LINE}";
+                }
+
+                string currentJob = workers[i].CurrentJob;
+                report += String.IsNullOrEmpty(currentJob)
+                    ? $"{worker} is not working{NEW_LINE}"
+                    : workers[i].ShiftsLeft > 0
+                        ? $"{worker} is doing `{currentJob}` for {workers[i].ShiftsLeft} more shifts{NEW_LINE}"
+                        : $"{worker} will be done with `{currentJob}` after this shift{NEW_LINE}";
+            }
+            return report;
+        }
+    }
+}
---
+++ Beehive/Worker.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Beehive
+{
+    class Worker
+    {
+        private string currentJob = "";
+        public string CurrentJob
+        {
+            get
+            {
+                return currentJob;
+            }
+        }
+
+        public int ShiftsLeft
+        {
+            get
+            {
+                return shiftsToWork - shiftsWorked;
+            }
+        }
+
+        private string[] jobsICanDo;
+        private int shiftsToWork;
+        private int shiftsWorked;
+
+        public Worker(string[] jobsICanDo)
+        {
+            this.jobsICanDo = jobsICanDo;
+        }
+
+        public bool DoThisJob(string job, int numberOfShifts)
+        {
+            bool result = false;
+            if (String.IsNullOrEmpty(currentJob) && jobsICanDo.Contains(job))
+            {
+                currentJob = jobsICanDo.FirstOrDefault(j => j.Contains(job));
+                this.shiftsToWork = numberOfShifts;
+                shiftsWorked = 0;
+                result = true;
+            }
+            return result;
+        }
+
+        public bool DidYouFinish()
+        {
+            bool result = false;
+            if (!String.IsNullOrEmpty(CurrentJob))
+            {
+                shiftsWorked++;
+                if (shiftsWorked > shiftsToWork)
+                {
+                    shiftsWorked = 0;
+                    shiftsToWork = 0;
+                    currentJob = "";
+                    result = true;
+                }
+            }
+            return result;
+        }
+    }
+}
